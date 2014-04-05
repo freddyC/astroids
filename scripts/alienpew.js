3e8244c5dbce6872601dfde1def0c40e041cb748
culedewd83 <jesserogers@brigham.net>
2014-04-05 17:15:53 -0600

    even mor alien stuff

---
+++ scripts/alienpew.js
@@ -0,0 +1,59 @@
+MYGAME.alienPew = function(spec, graphics) {
+  'use strict';
+
+
+  var that = {},
+    elapsedSeconds = 0,
+    pew = {
+      image: spec.image,
+      size: {
+        width : spec.radius * 2,
+        height : spec.radius * 2
+      },
+      radius: spec.radius,
+      center: spec.center,
+      speed: spec.speed,
+      direction: spec.direction,
+      rotation: 0,
+      lifetime: spec.lifetime
+    };
+
+  that.shouldBeDeleted = false;
+
+  that.getPewCircle = function() {
+    var circle = {
+           point: JSON.parse(JSON.stringify(pew.center)),
+           radius: pew.radius
+        };
+    
+    return circle;
+  };
+
+
+  that.update = function(elapsedTime) {
+    elapsedSeconds += elapsedTime / 1000;
+    if (elapsedSeconds >= pew.lifetime) {
+      that.shouldBeDeleted = true;
+    } else {
+      pew.center.x += Math.sin(pew.direction) * pew.speed * elapsedTime / 1000;
+      pew.center.y -= Math.cos(pew.direction) * pew.speed * elapsedTime / 1000;
+      if (pew.center.y - pew.size.height / 2 >= window.innerHeight) {
+        pew.center.y = -pew.size.height / 2;
+      } else if (pew.center.y + pew.size.height / 2 <= 0) {
+        pew.center.y = window.innerHeight + pew.size.height / 2;
+      }
+
+      if (pew.center.x - pew.size.width / 2 >= window.innerWidth) {
+        pew.center.x = -pew.size.width / 2;
+      } else if (pew.center.x + pew.size.width / 2 <= 0) {
+        pew.center.x = window.innerWidth + pew.size.width / 2;
+      }
+    }
+  };
+
+  that.render = function() {
+    graphics.drawImage(pew);
+  };
+
+  return that;
+};
--- scripts/alienship.js
+++ scripts/alienship.js
@@ -1,235 +1,80 @@
 MYGAME.alienShip = function(spec, graphics) {
   'use strict';
-
+					
   var that = {}
-    , secondsToNextFire
+    , secondsToNextFire = Random.nextGaussian(spec.shotFreqency.mean, spec.shotFreqency.stdDev)
+    , secondsSinceLastFire = 0
     , ship = {
         image: spec.image,
         direction: spec.direction,
+        rotation: spec.rotation,
         speed: spec.speed,
         collisionCircles: spec.collisionCircles,
-        size: {
-          width: spec.width,
-          height: spec.height
-        },
+        size: spec.size,
         center: spec.center,
         shotAccuracy : spec.shotAccuracy
       }
-    ;
+    , originalCenter = JSON.parse(JSON.stringify(spec.center))
+   ;
 
+  var pewHandler = function(elapsedTime) {
+	  secondsSinceLastFire += (elapsedTime / 1000);
+    if (secondsSinceLastFire >= secondsToNextFire) {
+    	secondsSinceLastFire = 0;
+    	secondsToNextFire = Random.nextGaussian(spec.shotFreqency.mean, spec.shotFreqency.stdDev);
+    	
+    	var angleToPlayerShip;
+    	if(ship.center.y > MYGAME.gameController.playerShip.getShipCenter().y) {
+    		angleToPlayerShip = angleFromTwoPoints(ship.center, MYGAME.gameController.playerShip.getShipCenter());
+    	} else {
+    		angleToPlayerShip = angleFromTwoPoints(ship.center, MYGAME.gameController.playerShip.getShipCenter()) + Math.PI;
+    	}
+    	
+    	var shotSpec = {
+    			image: MYGAME.images['images/greencircle.png'],
+    			radius: 5,
+    			center: that.getShipCenter(),
+    			speed: 300,
+    			direction: angleToPlayerShip,
+    			lifetime: 1
+    	};
+    	
+    	MYGAME.gameController.alienLasers.push(MYGAME.alienPew(shotSpec, graphics));
 
-  var laserHandler = function(elapsedTime) {
-    secondsSinceLastLaserFired += (elapsedTime / 1000);
-    if (shouldTryToFireLaser) {
-      shouldTryToFireLaser = false;
-      if (!MYGAME.gameController.playerShipIsInvincible && MYGAME.gameController.lasers.length < 6) {
-        if (secondsSinceLastLaserFired >= 0.3) {
-          secondsSinceLastLaserFired = 0;
-          var laserSpec = {
-            image: MYGAME.images['images/pew.png'],
-            speed: 700 + (ship.speed * 50 * Angles.halfAngleRatio(ship.direction, ship.rotation)),
-            direction: ship.rotation,
-            lifetime: 2,
-            size: {
-              width: 6,
-              height: 34
-            },
-            center: {
-              x: ship.center.x,
-              y: ship.center.y
-            }
-          };
-          MYGAME.gameController.lasers.push(MYGAME.laser(laserSpec, MYGAME.graphics));
-
-          var laserSnd = new Audio('sounds/pew.mp3');
-          laserSnd.volume = 0.3;
-          laserSnd.play();
-        }
-      }
     }
   };
 
   var moveShip = function(elapsedTime) {
     var elapsedSeconds = elapsedTime / 1000;
-
-    if (isTurningLeft) {
-      ship.rotation -= elapsedSeconds * 5;
-    }
-
-    if (isTurningRight) {
-      ship.rotation += elapsedSeconds * 5;
-    }
-
-    if (isAccelerating) {
-      var Xspeed = Math.sin(ship.direction) * ship.speed
-        , Yspeed = Math.cos(ship.direction) * ship.speed
-        , Xforce = Math.sin(ship.rotation) * ship.acceleration * elapsedSeconds
-        , Yforce = Math.cos(ship.rotation) * ship.acceleration * elapsedSeconds
-        , eSpec
-        ;
-
-      ship.speed = Math.sqrt(Math.pow((Xspeed + Xforce),2) + Math.pow((Yspeed + Yforce),2));
-      ship.direction = (Math.atan((Xspeed + Xforce)/(Yspeed + Yforce))) % (2*Math.PI);
-
-      if (Yspeed + Yforce < 0) {
-        ship.direction = ship.direction + Math.PI;
-      }
-
-      var speedOffset = 0;
-
-      if (Angles.halfAngleRatio(ship.direction, ship.rotation) < 0) {
-        speedOffset = (ship.speed * 50 * Angles.halfAngleRatio(ship.direction, ship.rotation));
-      }
-
-      eSpec = {
-        shipFacing: ship.rotation,
-        speed: {
-          mean: 100 - speedOffset,
-          stdev: 25
-        },
-        center: {
-          x: ship.center.x + Math.sin(ship.rotation + (Math.PI * 1.225)) * (ship.size.width / 1.58),
-          y: ship.center.y - Math.cos(ship.rotation + (Math.PI * 1.225)) * (ship.size.height / 1.58)
-        }
-      };
-
-      for (var i = 0; i < 15; ++i) {
-        engine1.create(eSpec);
-      }
-
-      eSpec = {
-        shipFacing: ship.rotation,
-        speed: {
-          mean: 100 - speedOffset,
-          stdev: 25
-        },
-        center: {
-          x: ship.center.x + Math.sin(ship.rotation - Math.PI * 4.9 / 4) * ship.size.width/1.58,
-          y: ship.center.y - Math.cos(ship.rotation - Math.PI * 4.9 / 4) * ship.size.height/1.58
-        }
-      };
-
-      for (var i = 0; i < 15; ++i) {
-        engine2.create(eSpec);
-      }
-
-    } else {
-      //ship.speed = ship.speed - ship.acceleration * elapsedSeconds / 8;
-      if (ship.speed < 0) {
-        ship.speed = 0;
-      }
-    }
-
-    if (ship.speed > 10) {
-    ship.speed = 10;
-  }
-
-    ship.center.x += Math.sin(ship.direction) * ship.speed;
-    ship.center.y -= Math.cos(ship.direction) * ship.speed;
-
-    if (ship.center.y - ship.size.height/2 >= window.innerHeight) {
-      ship.center.y =  -ship.size.height/2;
-    } else if (ship.center.y + ship.size.height/2 <= 0) {
-      ship.center.y = window.innerHeight + ship.size.height/2;
-    }
-
-    if (ship.center.x - ship.size.width/2 >= window.innerWidth) {
-      ship.center.x =  -ship.size.width/2;
-    } else if (ship.center.x + ship.size.width/2 <= 0) {
-      ship.center.x = window.innerWidth + ship.size.width/2;
+    ship.center.x += Math.sin(ship.direction) * ship.speed * elapsedSeconds;
+    ship.center.y = originalCenter.y + (Math.cos(ship.center.x / 20) * 30);
+    if (ship.center.x <= ship.size.width / 2 || ship.center.x + ship.size.width / 2 > window.innerWidth) {
+    	ship.direction += Math.PI;
     }
   };
-
-  rocketSnd = new Audio('sounds/rocket.mp3');
-  rocketSnd.volume = 0.4;
-
-  engineSpec = {
-    image: MYGAME.images['images/blue.png'],
-    lifetime: { mean: 1.0, stdev: 0.5 }
-  };
-
-  engine1 = MYGAME.playerShipEngine(engineSpec, MYGAME.graphics);
-  engine2 = MYGAME.playerShipEngine(engineSpec, MYGAME.graphics);
-
-  myKeyboard.registerCommand(KeyEvent.DOM_VK_W, shipShouldAccel);
-  myKeyboard.registerCommand(KeyEvent.DOM_VK_A, shipShouldTurnLeft);
-  myKeyboard.registerCommand(KeyEvent.DOM_VK_D, shipShouldTurnRight);
-  myKeyboard.registerCommand(KeyEvent.DOM_VK_SPACE, fireLaserKeyPressed);
-
+  
   that.getShipCenter = function () {
-    // copy an object content to new object
     return JSON.parse(JSON.stringify(ship.center));
   };
 
-  that.resetShip = function() {
-  ship.rotation = 0;
-  ship.direction = 0;
-  ship.speed = 0;
-  ship.center = { x: window.innerWidth / 2,
-              y: window.innerHeight / 2
-            };
-  };
-
-  that.getShipPolygon = function() {
-    var poly = [], i, x, y;
+  that.getShipCollisionCircles = function() {
+    var circles = [], i, x, y;
     for(i = 0; i < ship.polyPoints.length;i++ ) {
-      x = ship.center.x + Math.sin(ship.rotation + ship.polyPoints[i].angle) * ship.polyPoints[i].distance;
-      y = ship.center.y + Math.cos(ship.rotation + ship.polyPoints[i].angle) * ship.polyPoints[i].distance;
-      poly.push({x:x,y:y});
+      x = ship.center.x + Math.sin(ship.rotation + ship.collisionCircles[i].angle) * ship.collisionCircles[i].distance;
+      y = ship.center.y + Math.cos(ship.rotation + ship.collisionCircles[i].angle) * ship.collisionCircles[i].distance;
+      circles.push({point:{x:x,y:y},radius:ship.collisionCircles[i].radius});
     }
-    return poly;
+    return circles;
   };
 
   that.update = function(elapsedTime) {
-    if(!MYGAME.gameController.playerShipShouldAppear) {
-      return;
-    }
-
-    // Update ship code here
-    isAccelerating = false;
-    isTurningLeft = false;
-    isTurningRight = false;
-    myKeyboard.update(elapsedTime);
     moveShip(elapsedTime);
-    engine1.update(elapsedTime/1000);
-    engine2.update(elapsedTime/1000);
-
-    if (isAccelerating !== isPlayingRocketSound) {
-      isPlayingRocketSound = isAccelerating;
-      if (isPlayingRocketSound) {
-        rocketSnd.play();
-      } else {
-        rocketSnd.pause();
-        rocketSnd.currentTime = 0;
-        rocketSoundSecondsPlayed = 0;
-      }
-    }
-
-    if (isPlayingRocketSound) {
-      rocketSoundSecondsPlayed += elapsedTime/1000;
-      if (rocketSoundSecondsPlayed >= 20) {
-        rocketSoundSecondsPlayed = 0;
-        rocketSnd.pause();
-        rocketSnd.currentTime = 0;
-        rocketSnd.play();
-      }
-    }
-
-    laserHandler(elapsedTime);
+    pewHandler(elapsedTime);
   };
 
   that.render = function() {
-  if (MYGAME.gameController.playerShipShouldAppear) {
-    if (MYGAME.gameController.playerShipIsInvincible) {
-    ship.fade = 0.5;
-    } else {
-    ship.fade = 1.0;
-    }
-      graphics.drawImage(ship);
-      engine1.render();
-      engine2.render();
-  }
+    graphics.drawImage(ship);
   };
-
+  
   return that;
 };
